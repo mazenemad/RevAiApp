899d7c286a19d90efe8959ad9f4992e79c0a6597
Mazen Emad <mazen2m9@gmail.com>
2024-06-24 12:59:28 +0300

    rev ai functions added

--- index.js
+++ index.js
@@ -10,13 +10,65 @@ app.use(cors());
 // Middleware to parse JSON bodies
 app.use(e.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const token = `02wQv02mBmqK-7REQJIWuPZ2kB45y4IVoPeAQU6LCLX_P3mziQ_RCeKwU6_tIclsKwu9NJ0Qw2IkG06TG3_pXSHwm48_8`
 
+async function Request(url, Auth) {
+  let data = ({
+    "source_config": {
+      "url": url
+    }
+  });
 
-app.post('/api/ask', async (req, res) => {
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://api.rev.ai/speechtotext/v1/jobs',
+    headers: {
+      'Authorization': `Bearer ${Auth}`,
+      'Content-Type': 'application/json'
+    },
+    data: data
+  };
+
+  let req = await axios.request(config)
+
+  return req.data
+}
+
+const GetText = async (id,Auth) => {
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `https://api.rev.ai/speechtotext/v1/jobs/${id}/transcript`,
+    headers: {
+      'Authorization': `Bearer ${Auth}`,
+      'Accept': 'text/plain'
+    }
+  };
+  let res = await axios.request(config)
+  console.log(res)
+  return res.data
+
+}
+
+app.post('/api/requestTrans', async (req, res) => {
   const { url, Auth } = req.body;
+  try {
+    let data = await Request(url, Auth)
+    res.json({ message: { "data": data } })
+  } catch (error) {
+    res.json({ message: { "error": error } })
+  }
+});
 
-  res.json({ message: { url, Auth } })
-
+app.get('/api/getData', async (req, res) => {
+  const { id, Auth } = req.body;
+  try {
+    let data = await GetText(id, Auth)
+    res.json({ message: { "data": data } })
+  } catch (error) {
+    res.json({ message: { "error": error } })
+  }
 });
 
 // Start the server
