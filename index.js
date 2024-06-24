dbfbc49ea17904bf3e500b8b23a8867ff654a05f
Mazen Emad <mazen2m9@gmail.com>
2024-06-24 13:23:33 +0300

    solve4

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ import e from 'express';
 import axios from 'axios';
 import cors from 'cors'
 import bodyParser from 'body-parser';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 const app = e();
 const port = 5000;
 app.use(cors());
 // Middleware to parse JSON bodies
 app.use(e.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api', createProxyMiddleware({ target: 'https://ao.bot', changeOrigin: true }));
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.send();
-});
+// app.use('/api', createProxyMiddleware({ target: 'https://ao.bot', changeOrigin: true }));
+// app.options('*', (req, res) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.send();
+// });
 const token = `02wQv02mBmqK-7REQJIWuPZ2kB45y4IVoPeAQU6LCLX_P3mziQ_RCeKwU6_tIclsKwu9NJ0Qw2IkG06TG3_pXSHwm48_8`
 
 async function Request(url, Auth) {
@@ -39,7 +40,7 @@ async function Request(url, Auth) {
 
   let req = await axios.request(config)
 
-  return req.data
+  return req.data || req
 }
 
 const GetText = async (id, Auth) => {
@@ -53,21 +54,42 @@ const GetText = async (id, Auth) => {
     }
   };
   let res = await axios.request(config)
-  return res.data
+  console.log(res)
+  return res?.data || res
 
 }
 
 app.post('/api/requestTrans', async (req, res) => {
   const { url, Auth } = req.body;
+  if (!url || !Auth)
+    res.json({ message: { "error": 'please provide' } })
   let data = await Request(url, Auth)
-  res.json({ message: { "data": data } })
-
+  try {
+    res.json({ message: { "data": data } })
+  } catch (error) {
+    res.json({ message: { "error": error } })
+  }
 });
 
 app.get('/api/getData', async (req, res) => {
   const { id, Auth } = req.body;
+  if (!id || !Auth)
+    res.json({ message: { "error": 'please provide' } })
+  console.log(id, Auth)
   let data = await GetText(id, Auth)
-  res.json({ message: { "data": data } })
+  try {
+    res.json({ message: { "data": data } })
+  } catch (error) {
+    res.json({ message: { "error": error } })
+  }
+});
+
+app.post('/api/test', async (req, res) => {
+  const { id, Auth } = req.body;
+  if (!id || !Auth)
+    res.json({ message: { "error": 'please provide' } })
+  else
+    res.json({ message: { "message": { id, Auth } } })
 
 });
 
